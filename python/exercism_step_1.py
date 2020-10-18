1119a52883790d80dfe4e5d381d1502efe476176
MaxKonovalov38 <max.kon.38r@gmail.com>
2020-10-18 16:35:45 +0800

    Добавлена и описана функция main

---
+++ python/exercism_step_1.py
@@ -0,0 +1,88 @@
+"""
+Conversion functions for the NATO Phonetic Alphabet.
+"""
+
+# To save a lot of typing the code words are presented here
+# as a dict, but feel free to change this if you'd like.
+ALPHANUM_TO_NATO = {
+    "A": "ALFA",
+    "B": "BRAVO",
+    "C": "CHARLIE",
+    "D": "DELTA",
+    "E": "ECHO",
+    "F": "FOXTROT",
+    "G": "GOLF",
+    "H": "HOTEL",
+    "I": "INDIA",
+    "J": "JULIETT",
+    "K": "KILO",
+    "L": "LIMA",
+    "M": "MIKE",
+    "N": "NOVEMBER",
+    "O": "OSCAR",
+    "P": "PAPA",
+    "Q": "QUEBEC",
+    "R": "ROMEO",
+    "S": "SIERRA",
+    "T": "TANGO",
+    "U": "UNIFORM",
+    "V": "VICTOR",
+    "W": "WHISKEY",
+    "X": "XRAY",
+    "Y": "YANKEE",
+    "Z": "ZULU",
+    "0": "ZERO",
+    "1": "ONE",
+    "2": "TWO",
+    "3": "TREE",
+    "4": "FOUR",
+    "5": "FIVE",
+    "6": "SIX",
+    "7": "SEVEN",
+    "8": "EIGHT",
+    "9": "NINER",
+}
+
+def transmit(message: str) -> str:
+    """
+    Convert a message to a NATO code word transmission.
+    """
+    pass  # <- implement your function
+
+
+def receive(transmission: str) -> str:
+    """
+    Convert a NATO code word transmission to a message.
+    """
+    pass  # <- implement your function
+
+
+def redacting_str(row_line:str) -> str:
+    '''
+    Функция redacting_str() редактирует строку,
+    убрав из неё знаки препинания и пробелы.
+    '''
+    pass
+
+
+def main():
+    '''
+    Функция main() принимает от пользователя сообщение, а
+    также режим кодировать/раскодировать (E/D).
+    '''
+    start_message = input("Write the message: ").upper()
+    crypt_mode = input("[E]ncrypt|[Decrypt]: ").upper()
+
+    row_message = redacting_str(start_message)
+
+    if crypt_mode == 'E':
+        transmit(row_message)
+    elif crypt_mode == 'D':
+        receive(row_message)
+    else:
+        print("Error: parametr is not found!!!")
+        raise SystemExit
+
+
+if __name__ == '__main__':
+    main()
